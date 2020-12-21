0c11ecc1f05bb57d927c8e5f117474d2f14ee492
hollowly <2499538506@qq.com>
2020-12-22 02:00:02 +0800

    enter

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -13,8 +13,25 @@
 			<a href=""><img src="@/assets/img/index/tmall3.jpg"></a>
 		</div>
 
-		<div>
-			<img src="@/assets/svg/logo-msr-new.svg">
+		<div id='program'>
+			<div class="program-left">
+				<h2 style="font-weight:500">星享俱乐部</h2>
+				<p>
+					开启您的星享之旅，星星越多、会员等级越高、
+				</p>
+				<p>
+					好礼越丰富。<a href="" style="color: #C2A661;">了解更多 › </a>
+				</p>
+				<a href='/register' class="aInput">注册</a>
+				<a href='/login' class="aInput login-in">登录</a>
+			</div>
+			<div>
+				<img src="@/assets/svg/logo-msr-new.svg">
+			</div>
+		</div>
+
+		<div id='tmall'>
+
 		</div>
 	</main>
 </template>
@@ -89,5 +106,44 @@ export default {
 		border-radius: 15px;
 		margin: 20px 0px 20px 25px;
 		transition: all .3s;
+	}
+
+	#program {
+		width: 73%;
+		height: 210px;
+		background: white;
+		margin: 0 auto;
+		padding: 40px 0px;
+		line-height: 300%;
+		border: 1px solid red;
+	}
+	#program > :first-child {
+		width: 50%;
+		height: 100%;
+		float: left;
+		/* border: 1px solid yellow; */
+	}
+	#program > :first-child > p {
+		color: rgba(0, 0, 0, 0.56);
+	}
+	#program .aInput {
+		padding: 3px 12px;
+		font-size: 20px;
+		text-align: center;
+		margin: 0px 10px 0px 0px;
+		border: 1px solid #00a862;
+		border-radius: 48px;
+		color: #00a862;
+		background: white;
+		outline: none;
+		cursor: pointer;
+		font-weight: 300;
+	}
+	#program > :last-child {
+		float: right;
+		width: 35%;
+		height: 100%;
+		padding-top: 65px;
+		/* border: 1px solid blue; */
 	}
 </style>
