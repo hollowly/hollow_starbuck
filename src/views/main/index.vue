f995bf3fd7798264cbc7d5de07acb7d8addce0c6
hollowly <2499538506@qq.com>
2020-12-24 18:41:13 +0800

    enter

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -47,6 +47,7 @@
 				</li>
 			</ul>
 		</div>
+
 		<div id='tmall' style="background:rgb(253,253,253)">
 			<div class="tmall-top">
 				<h2 >1912 派克街 | 咖啡星讲堂</h2><br>
@@ -54,9 +55,17 @@
 					了解更多星巴克咖啡文化
 				</p>
 			</div>
-			<ul>
+			
+			<div class="tmall-screen">
+				<span class="screen-right"><img src="@/assets/svg/screen-right.svg'"></span>
+				<a href="" v-for='item in tmallCulture'>
+					<div :style="{backgroundImage: 'url(' + item.imgUrl + ')'}">
+						<span>{{item.title}}</span>
+					</div>
+					<p>{{item.p}}</p>
+				</a>
+			</div>
 
-			</ul>
 		</div>
 
 		
@@ -78,6 +87,12 @@ export default {
 				{title:'星礼卡',p1:'用一份心礼',p2:'让心意相随',imgUrl:require('@/assets/img/index/tmall2.png')},
 				{title:'电子产品券',p1:'心意',p2:'从这一杯开始',imgUrl:require('@/assets/img/index/tmall3.png')},
 				{title:'咖啡生活',p1:'星巴克',p2:'用心制作',imgUrl:require('@/assets/img/index/tmall4.png')},
+			],
+			tmallCulture: [
+				{title:'咖啡知识',p:'咖啡的起源于栽培',imgUrl:require('@/assets/img/index/coffeehouse1.jpg')},
+				{title:'咖啡品鉴',p:'咖啡调制',imgUrl:require('@/assets/img/index/coffeehouse2.jpg')},
+				{title:'咖啡知识',p:'咖啡烘焙',imgUrl:require('@/assets/img/index/coffeehouse3.jpg')},
+				{title:'咖啡品鉴',p:'手冲咖啡',imgUrl:require('@/assets/img/index/coffeehouse4.jpg')},
 			]
 		}
 	},
@@ -200,6 +215,17 @@ export default {
 		height: 430px;
 		float: right;
 	}
+	.screen-right {
+		display: inline-block;
+		width: 30px;
+		height: 30px;
+		border-radius: 30px;
+		border: 1px solid red;	
+		position: relative;
+		left: 1000px;
+		top: -80px;
+		
+	}
 	#tmall > .tmall-top {
 		width: 100%;
 		height: 100px;
@@ -244,5 +270,36 @@ export default {
 	}
 	#tmall > ul > li > p > a:hover {
 		opacity: 0.7;
+	}
+
+	#tmall > .tmall-screen {
+		width: 200%;
+		height: 50%;
+	}
+	#tmall > .tmall-screen > a {
+		box-shadow: 0 2px 3px 1px rgba(0, 0, 0, 0.12);
+		width: 14%;
+		height: 100%;
+		margin: 0px 10px 0px 10px;
+		display: inline-block;
+	}
+	#tmall > .tmall-screen > a > p {
+		color: black;
+		font-size: 16px;
+		line-height: 50px;
+		text-indent: 1rem;
+	}
+	#tmall > .tmall-screen > a > div {
+		width: 100%;
+		height: 75%;
+		background-size: 100%;
+	}
+	#tmall > .tmall-screen > a > div > span {
+		font-size: 14px;
+		display: inline-block;
+		margin-top: 8px;
+		padding: 8px 12px;
+    background: #C2A661;
+    color: #FFF;
 	}
 </style>
