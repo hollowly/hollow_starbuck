dab59b33981e58c7e0045a63bf81a173a93241d0
hollowly <2499538506@qq.com>
2021-01-09 16:38:00 +0800

    enter

---
+++ src/components/main/user/indexframe.vue
@@ -0,0 +1,118 @@
+<!--  -->
+<template>		
+<div class="container title">
+	<div class="row">
+		<div class="col-sm">
+			<slot name='titletext'></slot>
+		</div>
+		<div class="col-sm titleright">
+			<slot name='titlenum'></slot>
+			<slot name='titleimg'></slot>
+		</div>
+	</div>
+
+	<slot name='main'>
+	</slot>
+
+	<div class="row footter">
+		<div class="col-sm">
+		</div>
+		<div class="col-sm">
+			<slot name='footterUrl'></slot>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	},
+	mounted() {
+		$('.main > .scroll > .card-item').mouseenter(function() {
+			$(this).css('transform','translateY(-20px)')
+		}).mouseleave(function() {
+			$(this).css('transform','translateY(0px)')
+		})
+	}
+
+}
+
+</script>
+
+<style scoped>
+.title {
+	margin-top: 30px;
+	width: 70%;
+	background: white;
+	font-size: 22px;
+	box-shadow: 0 1px 1px 1px rgba(0,0,0,.12);
+}
+.title > div {
+	padding: 18px 40px;
+	border-bottom: 1px solid rgba(0,0,0,.12);
+}
+.title > div > .titleright {
+	text-align: right;
+}
+.title > div > .titleright > img {
+	margin-bottom: 5px;
+}
+.title > div > div > h4 {
+	margin-top: 4px;
+}
+
+.main > .scroll {
+	height: 280px;
+	overflow-y: hidden;
+	padding-top: 15px;
+}
+
+.main > .scroll > .card-item {
+	transition: all .4s;
+	float: left;
+	margin-right: 30px;
+}
+.main > .scroll > .card-item > p {
+	padding: 0;
+	margin: 0;
+}
+.main > .scroll > .card-item > img {
+	width: 280px;
+}
+.main > .scroll > .card-item > span {
+		display: block;
+    margin-top: 12px;
+    margin-bottom: 17px;
+    text-align: center;
+    font-size: 12px;
+    font-weight: 400;
+    color: rgba(0,0,0,.38);
+    line-height: 160%;
+}
+@media screen and (max-width: 1213px) {
+		.title {
+			width: 80%;
+		}
+		.main > .line > div:last-child {
+		left: 82% !important;
+		}
+	}
+
+
+
+
+
+
+.footter {
+	text-align: right;
+	font-size: 16px;
+	color: #00a862 !important;
+}
+.footter > div > a {
+	color: #00a862 !important;
+}
+</style>
--- src/views/main/user/account.vue
+++ src/views/main/user/account.vue
@@ -2,40 +2,59 @@
 <template>
 	<div style="width:100%; height:2000px; background:rgb(248,248,248)">
 		<wrapper></wrapper>
-
-		<div class="container title">
-			<div class="row">
-				<div class="col-sm">
-					管理星礼卡
-				</div>
-				<div class="col-sm titleright">
-					<strong>24</strong>	<img src="@/assets/svg/user/index/card-title.svg" width="30"> 
-				</div>
-			</div>
-
-			<div class="row main">
-				<div class="col-sm scroll">
-					<div class="card-item">
-						<p>￥200</p>
-						<img src="@/assets/img/main/user/star_gift_card/1.jpg">
-						<span>7310 2332 6124 1994</span>
-					</div>
-					<div class="card-item">
-						<p>￥200</p>
-						<img src="@/assets/img/main/user/star_gift_card/2.jpg">
-						<span>7310 2332 6124 1994</span>
+		<indexframe>
+			<h4 slot='titletext'>管理星礼卡</h4>
+			<strong slot='titlenum'>24</strong>
+			<img src="@/assets/svg/user/index/card-title.svg" width="30" slot='titleimg'>
+			<div slot='main'>
+				<div class="row main">
+					<div class="col-sm scroll">
+						<div class="card-item">
+							<p>￥200</p>
+							<img src="@/assets/img/main/user/star_gift_card/1.jpg">
+							<span>7310 2332 6124 1994</span>
+						</div>
+						<div class="card-item">
+							<p>￥200</p>
+							<img src="@/assets/img/main/user/star_gift_card/2.jpg">
+							<span>7310 2332 6124 1994</span>
+						</div>
 					</div>
 				</div>
 			</div>
+			<router-link to="/account/Star-gift-card" slot='footterUrl'>查看所有卡片</router-link>
+		</indexframe>
 
-			<div class="row footter">
-				<div class="col-sm">
-				</div>
-				<div class="col-sm">
-					<router-link to="/account/Star-gift-card">查看所有卡片</router-link>
-				</div>
+		<indexframe>
+			<h4 slot='titletext'>星享好礼</h4>
+			<strong slot='titlenum'>6</strong>
+			<img src="@/assets/svg/user/index/gift.svg" width="30" slot='titleimg'>
+			<div slot="main">
+				<ul class="mainul">
+					<li v-for='item in star_gift'>
+						<img :src="item.imgUrl">
+						<span>{{item.titletext}}</span><br><span>{{item.date}}</span>
+					</li>
+				</ul>
 			</div>
-		</div>
+			<router-link to="/account/rewards" slot='footterUrl'>查看好礼详情</router-link>
+		</indexframe>
+		
+		<indexframe>
+			<h4 slot='titletext'>消费记录</h4>
+			<img src="@/assets/svg/user/index/cardlittle.svg" width="30" slot='titleimg'>
+			<div slot="main">
+				<ul class="mainul">
+					<li v-for='item in records_consumption'>
+						<img :src="item.imgUrl">
+						<span style="float:right">{{item.money}}</span>
+						<span>{{item.titletext}}</span><br><span>{{item.date}}</span>
+					</li>
+				</ul>
+			</div>
+			<router-link to="/account/rewards" slot='footterUrl'>查看更多</router-link>
+		</indexframe>
+
 
 
 	</div>
@@ -43,94 +62,52 @@
 
 <script>
 import wrapper from '@/components/main/user/wrapper'
+import indexframe from '@/components/main/user/indexframe'
 export default {
 	data () {
 		return {
-
+			star_gift: [
+				{imgUrl:require("@/assets/svg/user/index/glass-brown.svg"),titletext:'玉星晋级饮品券',date:'2021/01/10'},
+				{imgUrl:require("@/assets/svg/user/index/glass-brown.svg"),titletext:'星巴克玩味冰调™立减5元',date:'2021/01/10'},
+				{imgUrl:require("@/assets/svg/user/index/glass-brown.svg"),titletext:'星冰粽单包减28元',date:'2021/01/11'},
+				{imgUrl:require("@/assets/svg/user/index/glass-brown.svg"),titletext:'同笔交易两包或以上星冰粽减66元',date:'2021/01/11'},
+			],
+			records_consumption: [
+				{imgUrl:require("@/assets/svg/user/index/glass-black.svg"),titletext:'金桥佳邻坊店(Joy Link Hub Store)',date:'2021/01/3',money:'￥61.00'},
+				{imgUrl:require("@/assets/svg/user/index/glass-black.svg"),titletext:'金桥佳邻坊店(Joy Link Hub Store)',date:'2021/01/5',money:'￥35.00'},
+				{imgUrl:require("@/assets/svg/user/index/glass-black.svg"),titletext:'金桥佳邻坊店(Joy Link Hub Store)',date:'2021/01/6',money:'￥100.00'},
+				{imgUrl:require("@/assets/svg/user/index/glass-black.svg"),titletext:'金桥佳邻坊店(Joy Link Hub Store)',date:'2021/01/8',money:'￥38.00'},
+			]
 		}
 	},
 	components: {
-		wrapper
+		wrapper,
+		indexframe
 	},
-	mounted() {
-		$('.main > .scroll > .card-item').mouseenter(function() {
-			$(this).css('transform','translateY(-20px)')
-		}).mouseleave(function() {
-			$(this).css('transform','translateY(0px)')
-		})
-	}
-
 }
 
 </script>
-
 <style scoped>
-.title {
-	margin-top: 30px;
-	width: 70%;
-	background: white;
-	font-size: 22px;
-	box-shadow: 0 1px 1px 1px rgba(0,0,0,.12);
-}
-.title > div {
-	padding: 18px 40px;
-	border-bottom: 1px solid rgba(0,0,0,.12);
-}
-.title > div > .titleright {
-	text-align: right;
-}
-.title > div > .titleright > img {
-	margin-bottom: 5px;
-}
-
-.main > .scroll {
-	height: 280px;
-	overflow-y: hidden;
-	padding-top: 15px;
-}
-
-.main > .scroll > .card-item {
-	transition: all .4s;
-	float: left;
-	margin-right: 30px;
-}
-.main > .scroll > .card-item > p {
-	padding: 0;
-	margin: 0;
-}
-.main > .scroll > .card-item > img {
-	width: 280px;
-}
-.main > .scroll > .card-item > span {
-		display: block;
-    margin-top: 12px;
-    margin-bottom: 17px;
-    text-align: center;
-    font-size: 12px;
-    font-weight: 400;
-    color: rgba(0,0,0,.38);
-    line-height: 160%;
-}
-@media screen and (max-width: 1213px) {
-		.title {
-			width: 80%;
-		}
-		.main > .line > div:last-child {
-		left: 82% !important;
-		}
+	.mainul {
+		list-style: none;
 	}
-
-
-
-
-
-
-.footter {
-	text-align: right;
-	font-size: 16px;
-	color: #00a862 !important;
-}
-.footter > div > a {
-	color: #00a862 !important;
-}
-</style>	
+	.mainul > li {
+		padding: 10px 0px;
+		font-size: 16px;
+		border-bottom: 1px solid rgba(0,0,0,.12);
+	}
+	.mainul > li:first-child {
+		padding-top: 20px;
+	}
+	.mainul > li:last-child {
+		border-bottom: 0px;
+	}
+	.mainul > li > img {
+		width: 30px;
+		transform: translateY(10px);
+	}
+	.mainul > li > span:last-child {
+		font-size: 12px;
+		padding-left: 30px;
+	}
+</style>
