1bf62f20c37b88537ebe06edda0f1ba5d447b823
hollowly <2499538506@qq.com>
2021-01-12 15:25:59 +0800

    enter

--- src/components/nav/user.vue
+++ src/components/nav/user.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
 	<div id='profile'>
-		<h3 style="font-weight:700">晚上好！{{cookiename}}☕</h3>
+		<h3 style="font-weight:700">{{time}}{{cookiename}}☕</h3>
 		<ul>
 			<slot name='account'><li><router-link to='/account'>账户一览</router-link></li></slot>
 			<slot name='activity'><li><router-link to='/account/activity'>消费记录</router-link></li></slot>
@@ -20,6 +20,7 @@ export default {
 	data () {
 		return {
 			cookiename:null,
+			time:'',
 			menu: [
 				{url:'/account',text:''},
 				{url:'/account/',text:''},
@@ -34,6 +35,17 @@ export default {
 		this.cookiename = this.cookie.getCookie('loginname')
 		if(this.cookiename) {
 			this.isshow = true;
+			// 根据时间来渲染，早上好，中午好，晚上好
+			var hr = (new Date()).getHours();
+			if(hr >= 6 && hr < 11) {
+				this.time = '早上好！'
+			} else if(hr >= 11 && hr < 14) {
+				this.time = '中午好！'
+			} else if(hr >= 14 && hr <= 18) {
+				this.time = '下午好！'
+			} else {
+				this.time = '晚上好！'
+			}
 			return
 		}
 		// 判断没有登录就跳转到登录页面
@@ -41,6 +53,7 @@ export default {
 			console.log(this.cookiename);
 			this.$router.replace('/login')
 		}
+
 	}
 }
 </script>
